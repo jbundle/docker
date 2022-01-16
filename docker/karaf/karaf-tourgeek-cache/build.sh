0daf192c05faf3a8e731d618687561867fa15231
Don Corley <don@donandann.com>
2022-01-15 16:50:57 -0800

    Create cached karaf-tourgeek-cache

--- docker/karaf/karaf-tourgeek-cache/build.sh
+++ docker/karaf/karaf-tourgeek-cache/build.sh
@@ -7,11 +7,11 @@ docker rm karaf-tourgeek
 
 docker run -d --name karaf-tourgeek tourgeek/karaf-tourgeek:latest
 sleep 15
-docker exec -t karaf-tourgeek /opt/apache/karaf/bin/client shell:source mvn:org.jbundle.config/jbundle-artifacts//shell/setup
+docker exec -t karaf-tourgeek client shell:source mvn:org.jbundle.config/jbundle-artifacts//shell/setup
 sleep 5
-docker exec -t karaf-tourgeek /opt/apache/karaf/bin/stop
+docker exec -t karaf-tourgeek stop
 sleep 5
 
 docker commit karaf-tourgeek tourgeek/karaf-tourgeek-cache
-docker tag tourgeek/karaf-tourgeek-cache:latest tourgeek/karaf-tourgeek-cache:4.2.9
+docker tag tourgeek/karaf-tourgeek-cache:latest tourgeek/karaf-tourgeek-cache:1.4.1
 #docker push
